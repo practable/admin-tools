f7fb544ea995c9e1009d6c39aa38858691cc4b1b
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-08-18 17:33:22 +0100

    dev (feat): add client tokens for experiments

--- dev/experiments/jump/generate_all.sh
+++ dev/experiments/jump/generate_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-(cd ./scripts; ./configure pvna 00 05 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
-(cd ./scripts; ./configure pend 00 37 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
-(cd ./scripts; ./configure spin 30 77 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
-(cd ./scripts; ./configure trus 00 07 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./host pvna 00 05 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./host pend 00 37 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./host spin 30 77 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./host trus 00 07 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./client pvna 00 05 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./client pend 00 37 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./client spin 30 77 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
+(cd ./scripts; ./client trus 00 07 https://app.practable.io/dev/jump ~/secret/app.practable.io/dev/jump.pat)
