c3d8b633be6309c89181f32b079b22e50da87a9d
Tim Drysdale <timothy.d.drysdale@gmail.com>
2024-04-10 14:42:19 +0100

    ed0 (wip): ansible login regexp
    Ansible login regexp added which identifies the id we gave it in the
    inventory, but ansible cannot resolve the host name for the persisted
    connection, I think because we need to give ansible the actual
    host name and port so it can reconnect

--- ed0/jump/ansible-login.sh
+++ ed0/jump/ansible-login.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-# pass on all arguments to ssh
-# extract id using regexp ( [a-zA-Z0-9]+ ) (1st match first group, also finds sleep command in our proof of concept as first match in second group)
+# Usage ./ansible-login <options> <expt_id> [command]
+# Example usage: ansible-login.sh -vvv -C -o ControlMaster=auto -o ControlPersist=60s -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o ConnectTimeout=10 -o ControlPath=/home/tim/.ansible/cp/c235afd0de spin30 '/bin/sh -c '"'"'echo ~ && sleep 0'"'"''
 
-# Usage ./login <expt_id>
-# Example ./login pend00
-export EXPT=$1
+# split our arguments into options, expt id, and command
+target="^(.*?)( [a-zA-Z0-9]+ )(.*'.*')"
+echo "all opts: $*"
+
+[[ "$*" =~ $target ]]
+
+# debug only
+#    for i in "${!BASH_REMATCH[@]}"; do
+#        echo "$i: ${BASH_REMATCH[$i]}"
+#    done
+
+OPTIONS="${BASH_REMATCH[1]}"
+EXPT="${BASH_REMATCH[2]}"
+COMMAND="${BASH_REMATCH[3]}"
+
+#for debug only - upsets ansible if echo unexpected things
+#echo "options: $OPTIONS"
+#echo "expt: $EXPT"
+#echo "command: $COMMAND"
 
 function freeport(){
  #https://unix.stackexchange.com/questions/55913/whats-the-easiest-way-to-find-an-unused-local-port
@@ -33,8 +49,9 @@ pid=$!
 export SSHPASS=$(/home/tim/secret/ep $EXPT)
 user=$(/home/tim/secret/eu $EXPT)
 #sshpass -v -e ssh -o "StrictHostKeyChecking no" "${user}@localhost" -p "$port"
-sshpass -v -e ssh -C -o ControlMaster=auto -o ControlPersist=60s -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o ConnectTimeout=10 -o ControlPath=/home/tim/.ansible/cp/c235afd0de "${user}@localhost" -p "$port" '/bin/sh -c '"'"'echo ~ && sleep 0'"'"''
+#sshpass -v -e ssh -C -o ControlMaster=auto -o ControlPersist=60s -o KbdInteractiveAuthentication=no -o PreferredAuthentications=gssapi-with-mic,gssapi-keyex,hostbased,publickey -o PasswordAuthentication=no -o ConnectTimeout=10 -o ControlPath=/home/tim/.ansible/cp/c235afd0de "${user}@localhost" -p "$port" '/bin/sh -c '"'"'echo ~ && sleep 0'"'"''
 
+sshpass -v -e ssh $OPTIONS "${user}@localhost" -p "$port" $COMMAND
 
 kill $!
 
