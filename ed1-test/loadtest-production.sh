df84cb3f71f86be0cc85ee67788c64db44093ae9
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2025-12-16 14:58:51 +0000

    ed1-test (fix): loadtest script to do multiple users per expt

--- ed1-test/loadtest-production.sh
+++ ed1-test/loadtest-production.sh
@@ -9,21 +9,24 @@ declare -a last=(44 77 17 7 4)
 # get length of an array
 arraylength=${#code[@]}
 
-# use for loop to read all values and indexes
-for (( i=0; i<${arraylength}; i++ ));
+#double the number of feeds
+for i in $(seq 1 2);
 do
-  #echo "index: $i, code: ${code[$i]}, first: ${first[$i]}, last: ${last[$i]}"
-  for j in $(seq ${first[$i]} ${last[$i]})
-  do
-        num=$(printf "%02d" $j)
-        name="${code[$i]}${num}"
-        videoAccess=$(cat st-ed0-video.access.$name)
-        videoToken=$(cat st-ed0-video.token.$name)
-        curl -X POST -H "Content-Type: application/json" -d '{"stream":"'"video-${name}"'","destination":"'"${videoAccess}"'","id":"'"$((++id))"'","token":"'"${videoToken}"'"}' http://localhost:8888/api/destinations
-        dataAccess=$(cat st-ed0-data.access.$name)
-        dataToken=$(cat st-ed0-data.token.$name)
-        curl -X POST -H "Content-Type: application/json" -d '{"stream":"'"data-${name}"'","destination":"'"${dataAccess}"'","id":"'"$((++id))"'","token":"'"${dataToken}"'"}' http://localhost:8888/api/destinations
-  done
+    # use for loop to read all values and indexes
+    for (( i=0; i<${arraylength}; i++ ));
+    do
+    #echo "index: $i, code: ${code[$i]}, first: ${first[$i]}, last: ${last[$i]}"
+    for j in $(seq ${first[$i]} ${last[$i]})
+    do
+            num=$(printf "%02d" $j)
+            name="${code[$i]}${num}"
+            videoAccess=$(cat st-ed0-video.access.$name)
+            videoToken=$(cat st-ed0-video.token.$name)
+            curl -X POST -H "Content-Type: application/json" -d '{"stream":"'"video-${name}-$((++id))"'","destination":"'"${videoAccess}"'","id":"'"${id}"'","token":"'"${videoToken}"'"}' http://localhost:8888/api/destinations
+            dataAccess=$(cat st-ed0-data.access.$name)
+            dataToken=$(cat st-ed0-data.token.$name)
+            curl -X POST -H "Content-Type: application/json" -d '{"stream":"'"data-${name}-$((++id))"'","destination":"'"${dataAccess}"'","id":"'"${id}"'","token":"'"${dataToken}"'"}' http://localhost:8888/api/destinations
+    done
+    done
 done
-
 echo "Started $id feeds"
