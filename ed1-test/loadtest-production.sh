00c36d5aadf37a5474e830d6faaa68290bdf340e
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2025-12-16 14:00:13 +0000

    ed1-test (fix): loadtest rules to avoid reflecting traffic

--- ed1-test/loadtest-production.sh
+++ ed1-test/loadtest-production.sh
@@ -17,12 +17,12 @@ do
   do
         num=$(printf "%02d" $j)
         name="${code[$i]}${num}"
-        videoAccess=$(cat video.access.$name)
-        videoToken=$(cat video.token.$name)
-        curl -X POST -H "Content-Type: application/json" -d '{"stream":"video","destination":"'"${videoAccess}"'","id":"'"$((++id))"'","token":"'"${videoToken}"'"}' http://localhost:8888/api/destinations
-        dataAccess=$(cat data.access.$name)
-        dataToken=$(cat data.token.$name)
-        curl -X POST -H "Content-Type: application/json" -d '{"stream":"data","destination":"'"${dataAccess}"'","id":"'"$((++id))"'","token":"'"${dataToken}"'"}' http://localhost:8888/api/destinations
+        videoAccess=$(cat st-ed0-video.access.$name)
+        videoToken=$(cat st-ed0-video.token.$name)
+        curl -X POST -H "Content-Type: application/json" -d '{"stream":"'"video-${name}"'","destination":"'"${videoAccess}"'","id":"'"$((++id))"'","token":"'"${videoToken}"'"}' http://localhost:8888/api/destinations
+        dataAccess=$(cat st-ed0-data.access.$name)
+        dataToken=$(cat st-ed0-data.token.$name)
+        curl -X POST -H "Content-Type: application/json" -d '{"stream":"'"data-${name}"'","destination":"'"${dataAccess}"'","id":"'"$((++id))"'","token":"'"${dataToken}"'"}' http://localhost:8888/api/destinations
   done
 done
 
